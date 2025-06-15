5a86c2c75b93286e2144b690d983f7e5c6310e74
swathielangovan <swathielango23@gmail.com>
2025-06-15 16:08:00 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Python Playwright Starter Project
+
+This is a beginner friendly project using PlayWright for automation
+and testing
+
+## Setup
+````bash
+pip install -r requirements.txt
+playwright install
+pytest
+
+## 📌 Optional: Initialize Git & Push to GitHub
+
+```bash
+git init
+git add .
+git commit -m "Initial commit"
+gh repo create playwright-starter --public --source=. --push  # Requires GitHub CLI
---
+++ tests/test_sauceDemo.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import sync_playwright
+
+def test_saucedemo():
+    with sync_playwright() as sp:
+        browser = sp.chromium.launch(headless=False)
+        context = browser.new_context(viewport={"width":1200, "height":800})
+        page = browser.new_page()
+        page.goto("https://www.saucedemo.com/")
+        assert "Swag Labs" in page.title()
+
+        # Locate username and password
+        user_name = page.locator("input[name='user-name']")
+        pass_word = page.locator("input[name='password']")
+
+        # Send keys to the username & paswd
+        user_name.fill("standard_user")
+        print("Username entered!!")
+        pass_word.fill("secret_sauce")
+        print("Password entered")
+
+
+        # Locate Login btn and click it
+        login_btn = page.locator("input[type='submit']")
+        login_btn.click()
+        print("Successfully logged!!!")
+
+        assert "Products" in page.locator("body").inner_text()
+
+        browser.close()
